ca6e8fc0ad7e9c3d794973fd438c3bf60ffc7fc3
jiaweikoh <jiaweikoh@local.dev>
2025-07-08 04:47:52 +1000

    Scale UNICEF under-5 population values from thousands to absolute counts

--- gbd_pipeline_final.py
+++ gbd_pipeline_final.py
@@ -343,7 +343,7 @@ class UNICEFUnder5PopIndicator(Indicator):
                     year = time_lookup.get(t_idx)
                     if year is None:
                         continue
-                    value = obs[0]
+                    value = obs[0] * 1000  # API returns thousands of persons
                     records.append({"country": iso3, "year": year, "value": value})
 
             if not records:
